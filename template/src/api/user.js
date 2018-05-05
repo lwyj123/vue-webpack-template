5e8d55a93afed3f3c9fb367550cd48c7916fed67
liangyuyi <liangyuyi@bytedance.com>
2018-05-05 12:26:02 +0800

    feat: update into webpack 4

--- template/src/api/user.js
+++ template/src/api/user.js
@@ -1,5 +1,5 @@
 import fetch from 'utils/fetch'
-import store from 'vuex-store'
+import store from 'store'
 // get user details
 export function getUser () {
   return fetch({
--- template/src/store/index.js
+++ template/src/store/index.js
@@ -2,17 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import getters from './getters'
-import ls from '@/utils/localStorage'
-
-const LS_KEY = 'user' // localStorage key
-const lsData = ls.getItem(LS_KEY) // 获取本地数据
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    user,
-    movies
+    user
   },
   getters
   // plugins: [plugin]
