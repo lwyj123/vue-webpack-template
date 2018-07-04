f7a9dba1a1109800c645fbdc3baa4e9a949aece4
liangyuyi <liangyuyi@bytedance.com>
2018-07-04 16:23:20 +0800

    feat: delete some uncommon file

--- template/src/views/layout/Navbar/index.vue
+++ template/src/views/layout/Navbar/index.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="navbar">
     <div class="navbar-header">
-      <div class="logo" @click="$router.push({path: '/'})">午安影视</div>
-      <search-bar></search-bar>
-      <nav-user></nav-user>
-    </div>
-    <div class="navber-menu">
-      <nav-list></nav-list>
+      <div class="logo" @click="$router.push({path: '/'})">Website</div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import SearchBar from './SearchBar'
-import NavUser from './NavUser'
-import NavList from './NavList'
 
 export default {
-  components: {
-    'search-bar': SearchBar,
-    'nav-user': NavUser,
-    'nav-list': NavList
-  },
   data () {
     return {
     }
@@ -38,17 +25,6 @@ export default {
     },
     visibleChange () {
       this.isShowDrop = !this.isShowDrop
-    },
-    handleIconClick () {
-      if (this.input2 !== '') {
-        this.$store.commit('SET_SEARCHTEXT', this.searchContent)
-        this.$router.push({ path: '/search' })
-      } else {
-        alert('请输入要搜索的内容')
-      }
-    },
-    clickDot () {
-      this.dotShow = false
     }
   },
   mounted () {
@@ -77,10 +53,6 @@ export default {
       display: flex;
       align-items: center;
     }
-  }
-  .navber-menu{
-    height: 41px;
-    display: flex;
   }
 }
 </style>
